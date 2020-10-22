7ecdfce2b549f35cb04ef1304dcbe3560fc28090
unknown <hoanghan.qngai1999@gmail.com>
2020-10-22 11:25:10 +0700

    ham tinh hieu

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,23 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+	 int hieu = 0;
+	 hieu = a - b;
+	 return hieu;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a, b;
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT" << endl;
+	cout << "nhap a"<< endl; 
+	cin >> a;
+	cout << "nhap b" << endl;
+	cin >> b;
+	cout << "hieu cua a va b la: " <<hieu(a,b);
 	system("pause");
 	return 0;
 }
